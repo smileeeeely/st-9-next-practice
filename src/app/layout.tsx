d90bdab9f1d5d20b7de453221d0fda7d2a5d3653
smileeeeely <minjoole.ivy@gmail.com>
2025-03-14 11:37:58 +0900

    "1단계 완료"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <Link href="/">Home(SSG)</Link>
+          <Link href="/news">News(ISR)</Link>
+          <Link href="/blogs">Blogs(SSR)</Link>
+          <Link href="/todolist">TodoList(CSR)</Link>
+        </div>
         {children}
       </body>
     </html>
